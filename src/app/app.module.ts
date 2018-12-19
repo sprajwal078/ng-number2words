4478ca950d752cf98511f7f7b099b03d5454476e
prajwal078 <prajwal078@gmail.com>
2018-12-19 20:39:30 +1100

    enhancement(demo): import module and show demo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgNumber2wordsModule } from 'ng-number2words';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgNumber2wordsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
