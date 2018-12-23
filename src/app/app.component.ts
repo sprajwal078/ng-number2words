91c50c0ae73f9630e49f397a859631bdc6439826
prajwal078 <prajwal078@gmail.com>
2018-12-23 22:42:59 +1100

    enhancement(app component): enable demo for github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  value = 96749372;
+  language = 'en';
+  system = 'international';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NgNumber2wordsModule } from 'ng-number2words';
@@ -10,7 +11,8 @@ import { NgNumber2wordsModule } from 'ng-number2words';
   ],
   imports: [
     BrowserModule,
-    NgNumber2wordsModule
+    NgNumber2wordsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
